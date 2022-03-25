9e992993a89a225ee8bfa393fa4c9a845c41d615
Dakak-Takto <ifloop@ya.ru>
2022-03-25 14:44:24 +0500

    test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"log"
-	"yandex-practicum-go-shortener/config"
-	"yandex-practicum-go-shortener/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := CreateNewServer()
-	addr := config.GetAddr()
+	addr := "localhost:8080"
 	log.Printf("Start server on %s", addr)
 	log.Fatal(server.Run(addr))
 }
 
 func CreateNewServer() *gin.Engine {
 	server := gin.Default()
-	server.GET("/:key", handlers.GetHandler)
-	server.POST("/", handlers.PostHandler)
+	// server.GET("/:key", handlers.GetHandler)
+	// server.POST("/", handlers.PostHandler)
 	return server
 }
