81d51cac0374426fecb6ad5dc655296172066167
Dakak-Takto <ifloop@ya.ru>
2022-03-25 15:08:21 +0500

    testing

--- config/config.go
+++ config/config.go
@@ -1,34 +1,4 @@
 package config
 
-import (
-	"log"
-	"os"
-
-	"gopkg.in/yaml.v2"
-)
-
-type config struct {
-	ServerAddr string `yaml:"serverAddr"`
-	Scheme     string `yaml:"scheme"`
-}
-
-var cfg = config{}
-
-func init() {
-	file, err := os.ReadFile("config.yml")
-	if err != nil {
-		log.Fatalf("error while opening config file: %s", err)
-	}
-
-	err = yaml.Unmarshal(file, &cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func GetAddr() string {
-	return cfg.ServerAddr
-}
-func GetScheme() string {
-	return cfg.Scheme
-}
+var Addr = "localhost:8080"
+var Scheme = "http"
